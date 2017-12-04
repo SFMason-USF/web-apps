428cab0f3d21f69b2bb43b5470d56142c3e233b8
Spenser Mason <mason11@mail.usf.edu>
2017-12-04 18:05:42 -0500

    Fixed up data docs and added exception for malformed data

--- Final Exam/js/app.js
+++ Final Exam/js/app.js
@@ -54,7 +54,23 @@ $(function() {
           $("#search").removeClass("has-error");
         }
 
-        //Map an array of name data (a name datum being in the form: [name, 1900 rating, 1910 rating, etc.])
+        //Map name data provided by the server in the form:
+        // {
+        //   name: "name",
+        //   ratings: [
+        //     1900 rating,
+        //     1910 rating,
+        //     1920 rating,
+        //     1930 rating,
+        //     1940 rating,
+        //     1950 rating,
+        //     1960 rating,
+        //     1970 rating,
+        //     1980 rating,
+        //     1990 rating,
+        //     2000 rating
+        //   ]
+        // }
         //to an array that google charts can work with, namely:
         //[
         //  ["Year", name0, name1, etc.],
@@ -63,25 +79,13 @@ $(function() {
         //  ...
         //  ["2000", name0 rating for 2000, name1 rating for 2000, etc.]
         //]
-        //TODO: Change input data to format:
-        // {
-        //   name: "name",
-        //   ratings: [
-        //     rating,
-        //     rating,
-        //     rating,
-        //     rating,
-        //     rating,
-        //     rating,
-        //     rating,
-        //     rating,
-        //     rating,
-        //     rating,
-        //     rating
-        //   ]
-        // }
         console.log("rawData:", rawData);
         chartData[0].push(rawData.name);
+        if (chartData.length !== rawData.ratings.length + 1) {
+          throw new RangeError(
+            "Invalid data received from server! Incorrect number of ratings provided."
+          );
+        }
         for (let i = 1; i < chartData.length; ++i) {
           chartData[i].push(rawData.ratings[i - 1]);
         }
