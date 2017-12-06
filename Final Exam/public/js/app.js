b7b12cc988881220aa143adf65bfa66b1b3b86a8
Spenser Mason <mason11@mail.usf.edu>
2017-12-06 18:53:19 -0500

    Fixed chart formatting issue

--- Final Exam/public/js/app.js
+++ Final Exam/public/js/app.js
@@ -21,12 +21,16 @@ $(function() {
   google.charts.setOnLoadCallback(function() {
     const options = {
       hAxis: {
-        format: "none"
+        title: "Year"
       },
       height: 400,
       title: "Name Rankings",
-      vAxis: { title: "Rank", direction: -1, minValue: 0,
-          maxValue: 1001},
+      vAxis: {
+        title: "Rank",
+        direction: -1,
+        minValue: 0,
+        maxValue: 1001
+      },
       width: 600
     };
     const chart = new google.visualization.LineChart($("#graph")[0]);
@@ -38,58 +42,56 @@ $(function() {
     $("#search").on("change", function(event) {
       var name = event.target.value;
       $.ajax({
-        url: "names/" + name,
+        url: DEBUG ? "sample-data.json" : "names/" + name,
         method: "GET",
-        data: JSON
+        dataType: "json"
       }).done(function(rawData) {
         if (rawData) {
-          rawData = JSON.parse(rawData);
           $("#name-not-found")
-              .parent()
-              .hide();
+            .parent()
+            .hide();
           $("#search").removeClass("has-error");
-        //Map name data provided by the server in the form:
-        // {
-        //   name: "name",
-        //   ratings: [
-        //     1900 rating,
-        //     1910 rating,
-        //     1920 rating,
-        //     1930 rating,
-        //     1940 rating,
-        //     1950 rating,
-        //     1960 rating,
-        //     1970 rating,
-        //     1980 rating,
-        //     1990 rating,
-        //     2000 rating
-        //   ]
-        // }
-        //to an array that google charts can work with, namely:
-        //[
-        //  ["Year", name0, name1, etc.],
-        //  ["1900", name0 rating for 1900, name1 rating for 1900, etc.],
-        //  ["1910", name0 rating for 1910, name1 rating for 1910, etc.],
-        //  ...
-        //  ["2000", name0 rating for 2000, name1 rating for 2000, etc.]
-        //]
+          //Map name data provided by the server in the form:
+          // {
+          //   name: "name",
+          //   ratings: [
+          //     1900 rating,
+          //     1910 rating,
+          //     1920 rating,
+          //     1930 rating,
+          //     1940 rating,
+          //     1950 rating,
+          //     1960 rating,
+          //     1970 rating,
+          //     1980 rating,
+          //     1990 rating,
+          //     2000 rating
+          //   ]
+          // }
+          //to an array that google charts can work with, namely:
+          //[
+          //  ["Year", name0, name1, etc.],
+          //  ["1900", name0 rating for 1900, name1 rating for 1900, etc.],
+          //  ["1910", name0 rating for 1910, name1 rating for 1910, etc.],
+          //  ...
+          //  ["2000", name0 rating for 2000, name1 rating for 2000, etc.]
+          //]
 
-        chartData[0].push(rawData.name);
-        if (chartData.length !== (rawData.ratings).length + 1) {
-          throw new RangeError(
-            "Invalid data received from server! Incorrect number of ratings provided."
-          );
-        }
-        for (let i = 1; i < chartData.length; ++i) {
-          chartData[i].push(rawData.ratings[i - 1]);
-        }
-        chart.draw(google.visualization.arrayToDataTable(chartData), options);
-        }
-        else {
+          chartData[0].push(rawData.name);
+          if (chartData.length !== rawData.ratings.length + 1) {
+            throw new RangeError(
+              "Invalid data received from server! Incorrect number of ratings provided."
+            );
+          }
+          for (let i = 1; i < chartData.length; ++i) {
+            chartData[i].push(rawData.ratings[i - 1]);
+          }
+          chart.draw(google.visualization.arrayToDataTable(chartData), options);
+        } else {
           $("#name-not-found")
-              .text("No entered names found")
-              .parent()
-              .show();
+            .text("No entered names found")
+            .parent()
+            .show();
           $("#search").addClass("has-error");
         }
       });
